5903ef75b609cac6b24ca7460cdc3f13b1df23ae
Renan de Oliveira <85078612+renansouzzzz@users.noreply.github.com>
2023-03-13 10:35:18 -0300

    fix funcionario models

--- src/main/java/br/com/models/Funcionario.java
+++ src/main/java/br/com/models/Funcionario.java
@@ -8,8 +8,10 @@ public class Funcionario extends Pessoa {
 	private BigDecimal salario;
 	private String funcao;
 	
-	public Funcionario(String nome, LocalDate dataNascimento) {
+	public Funcionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) {
 		super(nome, dataNascimento);
+		this.salario = salario;
+		this.funcao = funcao;
 	}
 
 	public BigDecimal getSalario() {
